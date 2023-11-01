a7a5c900eef74bcda381aca0a81464dd93032b2e
Drummerzzz <jf.interatividade@gmail.com>
2023-11-01 10:24:53 -0300

    test: update tests

--- tests/pytelemetry_test.py
+++ tests/pytelemetry_test.py
@@ -1,6 +1,9 @@
+import uuid
+
 import pytest
 
-from pytelemetry import create_logger
+from pytelemetry import create_logger, set_trace_id
+from pytelemetry.context import PyTelemetryContextVar
 
 
 @pytest.mark.parametrize('level', ['info', 'debug', 'error'])
@@ -11,3 +14,15 @@ def test_pytelemetry_logger(level: str):
         getattr(logger, level)(message)
     except Exception as error:
         assert False, error
+
+
+@pytest.mark.parametrize('logger_name', ['invalid_name', None, 1])
+def test_pytelemetry_logger_not_fount(logger_name):
+    with pytest.raises(ValueError):
+        create_logger('Pytest', logger_name)
+
+
+def test_pytelemetry_trace_id():
+    trace_id = str(uuid.uuid4())
+    set_trace_id(trace_id)
+    assert trace_id == PyTelemetryContextVar.get_trace_id()
