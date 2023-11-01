88274216b0930fb2e2344b05856a080954c7cb6d
Drummerzzz <jf.interatividade@gmail.com>
2023-11-01 10:56:05 -0300

    test: update tests

--- tests/pytelemetry_test.py
+++ tests/pytelemetry_test.py
@@ -1,3 +1,4 @@
+import os
 import uuid
 
 import pytest
@@ -6,20 +7,18 @@ from pytelemetry import create_logger, set_trace_id
 from pytelemetry.context import PyTelemetryContextVar
 
 
+@pytest.mark.parametrize('save_to_file', [False, True])
 @pytest.mark.parametrize('level', ['info', 'debug', 'error'])
-def test_pytelemetry_logger(level: str):
-    message = f'Message by {level}'
-    logger = create_logger('Pytest')
+def test_pytelemetry_logger(level: str, save_to_file: bool):
+    if os.path.exists('app.log'):
+        os.remove('app.log')
+
+    logger = create_logger('Pytest', save_to_file=save_to_file)
     try:
-        getattr(logger, level)(message)
+        getattr(logger, level)(f'Message by {level}')
     except Exception as error:
         assert False, error
-
-
-@pytest.mark.parametrize('logger_name', ['invalid_name', None, 1])
-def test_pytelemetry_logger_not_fount(logger_name):
-    with pytest.raises(ValueError):
-        create_logger('Pytest', logger_name)
+    assert os.path.exists('app.log') == save_to_file
 
 
 def test_pytelemetry_trace_id():
