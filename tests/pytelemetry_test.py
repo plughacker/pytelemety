95497528c8e122b1b6d2543c6742a21db280f877
Drummerzzz <jf.interatividade@gmail.com>
2023-11-01 12:13:02 -0300

    tests: update tests

--- tests/pytelemetry_test.py
+++ tests/pytelemetry_test.py
@@ -1,5 +1,6 @@
 import os
 import uuid
+from unittest.mock import patch
 
 import pytest
 
@@ -12,13 +13,15 @@ from pytelemetry.context import PyTelemetryContextVar
 def test_pytelemetry_logger(level: str, save_to_file: bool):
     if os.path.exists('app.log'):
         os.remove('app.log')
+    with patch('pytelemetry.logger.LogConfig.is_logger_to_file') as mock:
+        mock.return_value = save_to_file
 
-    logger = create_logger('Pytest', save_to_file=save_to_file)
-    try:
-        getattr(logger, level)(f'Message by {level}')
-    except Exception as error:
-        assert False, error
-    assert os.path.exists('app.log') == save_to_file
+        logger = create_logger('Pytest')
+        try:
+            getattr(logger, level)(f'Message by {level}')
+            assert os.path.exists('app.log') == save_to_file
+        except Exception as error:
+            assert False, error
 
 
 def test_pytelemetry_trace_id():
